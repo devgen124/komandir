d53826631f17724a838195e163f41fce7b71efd2
Evgeniy Kolegov <devgen124@gmail.com>
2023-01-22 11:21:49 +0700

    filters mobile remade

--- public_html/wp-content/themes/komandir/sidebar-shop.php
+++ public_html/wp-content/themes/komandir/sidebar-shop.php
@@ -12,12 +12,10 @@
 <?php if (is_product_category() || is_product_tag()) :?>
 
 <aside id="woo-sidebar" class="widget-area filters-sidebar">
+    <h2>Фильтры</h2>
+    <?php //echo do_shortcode( '[br_filter_single filter_id=14543]' ); ?>
+    <button class="filters-close" aria-label="Закрыть"></button>
     <div class="filters-sidebar-inner">
-        <div class="filters-sidebar-header">
-            <h2>Фильтры</h2>
-            <?= do_shortcode( '[br_filter_single filter_id=14543]' ); ?>
-            <button class="filters-close" aria-label="Закрыть"></button>
-        </div>
         <?= do_shortcode( '[br_filters_group group_id=14536]' ); ?>
         <?= do_shortcode( '[br_filters_group group_id=14540]' ); ?>
         <?= do_shortcode( '[br_filters_group group_id=14508]' ); ?>
