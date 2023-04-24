c8db6e022dcce3d1137148d23901afdcc44d7fd9
Evgeniy Kolegov <kolegov@profit-lab.ru>
2023-04-24 09:22:25 +0700

    var_dump remove

--- public_html/wp-content/themes/komandir/woocommerce/cart/cart.php
+++ public_html/wp-content/themes/komandir/woocommerce/cart/cart.php
@@ -36,7 +36,6 @@ do_action( 'woocommerce_before_cart' ); ?>
             <?php
 
             foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
-				var_dump($cart_item['quantity']);
                 $_product   = apply_filters( 'woocommerce_cart_item_product', $cart_item['data'], $cart_item, $cart_item_key );
 				$product_id = apply_filters( 'woocommerce_cart_item_product_id', $cart_item['product_id'], $cart_item, $cart_item_key );
 
