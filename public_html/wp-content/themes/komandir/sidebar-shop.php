6ec37cdeac7cf6b46e06282a7e9ea240c7a146d2
Evgeniy Kolegov <devgen124@gmail.com>
2023-11-18 15:49:58 +0700

    filters

--- public_html/wp-content/themes/komandir/sidebar-shop.php
+++ public_html/wp-content/themes/komandir/sidebar-shop.php
@@ -9,18 +9,42 @@
 
 ?>
 
-<?php if ( is_product_category() || is_tax( 'promotion' ) || isset( $_GET['s'] ) ): ?>
+<?php
+$filter_ids = [
+	14508,
+	14536,
+	14540,
+	14828,
+	15420,
+	15447,
+	15469,
+	15485,
+	20096,
+	61298,
+	61319,
+	61332,
+	61340,
+	61571,
+	61583,
+	61593,
+	61610,
+	61626,
+	61678,
+	61690
+];
+
+?>
+
+<?php if ( ( is_product_category() || is_tax( 'promotion' ) || isset( $_GET['s'] ) ) && ( ! isset( $GLOBALS['hide_filters'] ) || $GLOBALS['hide_filters'] === false ) ): ?>
 
 	<aside id="woo-sidebar" class="widget-area filters-sidebar">
 		<h2>Фильтры</h2>
 		<button class="filters-close" aria-label="Закрыть"></button>
 		<div class="filters-sidebar-inner">
-			<?= do_shortcode( '[br_filters_group group_id=14536]' ); ?>
-			<?= do_shortcode( '[br_filters_group group_id=14540]' ); ?>
-			<?= do_shortcode( '[br_filters_group group_id=14508]' ); ?>
-			<?= do_shortcode( '[br_filters_group group_id=20096]' ); ?>
+			<?php foreach ( $filter_ids as $id ) {
+				echo do_shortcode( "[br_filters_group group_id=$id]" );
+			} ?>
 		</div>
 	</aside>
 
 <?php endif; ?>
-
