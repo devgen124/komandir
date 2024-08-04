002d03e9aca057e802c9a1967b65e0fea24cec05
Evgeniy Kolegov <devgen124@gmail.com>
2024-08-04 22:55:34 +0700

    убрал кастомизацию селекта для выбора региона для дефолного select2

--- source/js/custom_select.js
+++ source/js/custom_select.js
@@ -1,29 +1,30 @@
 import Select from "select-custom";
+
 export default function customizeSelects() {
 
-    initSelects();
+	initSelects();
 
-    $(document).on('berocket_ajax_products_loaded', initSelects);
+	$(document).on('berocket_ajax_products_loaded', initSelects);
 
-    function initSelects() {
-        const selects = document.querySelectorAll('select:not(.country_select)');
+	function initSelects() {
+		const selects = document.querySelectorAll('select:not(.country_select, .state_select)');
 
-        if (selects.length) {
-            selects.forEach((el) => {
-                const select = new Select(el, {
-                    optionBuilder: undefined,
-                    panelItem: { position: '', item: '', className: '' },
-                    changeOpenerText: true,
-                    multipleSelectionOnSingleClick: false,
-                    multipleSelectOpenerText: { labels: false, array: false },
-                    allowPanelClick: false,
-                    openOnHover: false,
-                    closeOnMouseleave: false,
-                    wrapDataAttributes: false,
-                    openerLabel: false,
-                });
-                select.init();
-            });
-        }
-    }
+		if (selects.length) {
+			selects.forEach((el) => {
+				const select = new Select(el, {
+					optionBuilder: undefined,
+					panelItem: {position: '', item: '', className: ''},
+					changeOpenerText: true,
+					multipleSelectionOnSingleClick: false,
+					multipleSelectOpenerText: {labels: false, array: false},
+					allowPanelClick: false,
+					openOnHover: false,
+					closeOnMouseleave: false,
+					wrapDataAttributes: false,
+					openerLabel: false,
+				});
+				select.init();
+			});
+		}
+	}
 }
